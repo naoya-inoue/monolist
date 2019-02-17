8723ba38ab16864f31e8fa8658bb4d9996323445
naoya.inoue <a.good.for.nothing.fellow@gmail.com>
2019-02-17 11:44:56 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,21 @@
+<header>
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="colllapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-left" href="/"><img src="{{ secure_asset("images/logo.png") }}" alt="Monolist"></a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar navbar-right">
+                    <li><a href="#">新規登録</a></li>
+                    <li><a href="#">ログイン</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
